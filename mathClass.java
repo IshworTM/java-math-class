5f4aa87a03352404a972d5baaddc00afd8fb3850
Ishwor <dulalishan3@gmail.com>
2024-06-28 21:11:52 +0545

    add main class
    Co-authored-by: Ishwor <dulalishan3@gmail.com>
    Co-authored-by: ish-py <dulalishan13@gmail.com>

---
+++ mathClass.java
@@ -0,0 +1,16 @@
+package ChapterNine.mathclass_examples;
+
+public class mathClass {
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-223));
+        System.out.println(Math.acos(2.23d));
+        System.out.println(Math.asin(6.23d));
+        System.out.println(Math.sqrt(144));
+        System.out.println(Math.cbrt(27));
+        System.out.println(Math.floor(3.43));
+        System.out.println(Math.log(7.56));
+        System.out.println(Math.ceil(5.55));
+        System.out.println(Math.atan(7.32));
+    }
+}
